df46f929fc3b3dad1da8f8e8472895919d356fa5
ScStew <scottmstewart2@gmail.com>
2017-08-29 09:12:06 -0400

    added test for valid keys

--- isbn.rb
+++ isbn.rb
@@ -3,12 +3,12 @@ def isbn_validator(isbn)
 	arr = isbn.split(//)
 	valid_keys = ["0","1","2","3","4","5","6","7","8","9","x"]
 	if arr.length == 10
-		#arr.each do |x|
-			#if valid_keys.include?(x)
+		arr.each do |x|
+			if valid_keys.include?(x)
 				true
-			#else
-				#false
-			#end
+			else
+				false
+			end
 
 
 	else
