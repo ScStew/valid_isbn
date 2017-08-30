cde61f23212ec874ba1628a50f8ef0844171fd52
ScStew <scottmstewart2@gmail.com>
2017-08-30 10:03:07 -0400

    split key checker for large and small and made test for small

--- isbn.rb
+++ isbn.rb
@@ -1,57 +1,53 @@
-def isbn_validator(isbn)
-	isbn.gsub!(/[^0-9A-Za-z]/, "")
+# def isbn_validator(isbn)
+# 	isbn_special = isbn.gsub!(/[^0-9A-Za-z]/, "")
+# 	if isbn_special.length == 10
+# 		if key_checker_small(isbn) == true
+# 			isbn.gsub!(/[^0-9A-Za-z]/, "")
+# 			arr = isbn.split(//)
+			
+# 			if arr.length == 10
+# 				checksum = math(arr)
+# 				if checksum == arr[-1]
+# 					true
+# 				else
+# 					false 
+# 				end
+# 			else
+# 				false
+# 		else
+# 			false
+# 		end	
+# 	elsif isbn_special.length == 13
+# 		if key_checker_large(isbn)
+# 			isbn.gsub!(/[^0-9A-Za-z]/, "")
+# 			arr = isbn.split(//)
+# 			if arr.length = 13
+# 				checksum = big_math(arr)
+# 				# p woo
+# 				# p arr
+# 				# p arr[-1]
+# 				if checksum == arr[-1]
+# 					true
+# 				else
+# 					false
+# 				end	
+# 			else
+# 				false
+# 			end
+			
+# 		else
+# 			false
+# 		end
+# 	else 
+# 		false
+		
+# 	end
+# end
+
+def key_checker_small(isbn)
 	arr = isbn.split(//)
-	
-	if arr.length == 10
-		if key_checker(arr) == true
-			woo = math(arr)
-			if woo == arr[-1]
-				true
-			else
-				false
-			end
-		else
-			false
-		end
-		
-
-	elsif arr.length == 13
-		if key_checker(arr) == true
-			woo = big_math(arr)
-			p woo
-			p arr
-			p arr[-1]
-			if woo == arr[-1]
-				true
-			else
-				false
-			end		
-		else 
-
-			false
-		end
-
-
-	else 
-		false
-		
-	end
-end
-
-def key_checker(arr)
 	true_arr = []
-	valid_keys = ["0","1","2","3","4","5","6","7","8","9","x"]
-	if arr.length == 10
-		arr.each do |x|
-			if valid_keys.include?(x)
-				true_arr << true
-			else
-				true_arr << false
-			end
-		end
-		
-	else
-		valid_keys.slice!(-1)
+	valid_keys = ["0","1","2","3","4","5","6","7","8","9","x","-"," "]
 		arr.each do |x|
 			if valid_keys.include?(x)
 				true_arr << true
@@ -60,55 +56,69 @@ def key_checker(arr)
 			end
 		end
 
-	end
 	if true_arr.include?(false)
 		false
 	else
 		true
 	end
+
 end
 
+# def key_checker_large(isbn)
+# 	arr = isbn.split(//)
+# 		true_arr = []	
+# 		valid_keys = ["0","1","2","3","4","5","6","7","8","9","-"," "]
+# 		arr.each do |x|
+# 			if valid_keys.include?(x)
+# 				true_arr << true
+# 			else
+# 				true_arr << false
+# 			end
+# 		end
+# 	if true_arr.include?(false)
+# 		false
+# 	else
+# 		true
+# 	end
+# end
 
-def math(arr)
+# def math(arr)
 
-	counter = 1
-	check = []
-	tick = arr.length-1
-	tick.times do
-		mult = arr[counter-1].to_i * counter
-		check << mult
-	counter += 1
-	end
+# 	counter = 1
+# 	check = []
+# 	tick = arr.length-1
+# 	tick.times do
+# 		mult = arr[counter-1].to_i * counter
+# 		check << mult
+# 	counter += 1
+# 	end
+# 	sum = check.sum	
+# 	checksum = sum%11
+# 	# p "#{checksum} checksum!!!!!!!!!!!!!!!!!"
+# 	if checksum == 10
+# 		checksum = "x"
+# 	else
+# 		checksum.to_s
+# 	end
+# end
 
-	sum = check.sum
-	
-	checksum = sum%11
-	# p "#{checksum} checksum!!!!!!!!!!!!!!!!!"
-	if checksum == 10
-		checksum = "x"
-	else
-		checksum.to_s
-	end
-end
-
-
-def big_math(arr)
-counter = 1
-adder = []
-	12.times do
-		if counter%2 == 0
-			mult = arr[counter-1].to_i*3
-			adder << mult
-		else
-			mult = arr[counter-1].to_i*1
-			adder << mult
-		end
-		counter += 1
-	end
-	# p adder
-	sum = adder.sum
-	y = sum % 10
-	x = 10 - y
-	checksum = y % 10
-	checksum.to_s
-end
+# def big_math(arr)
+# counter = 1
+# adder = []
+# 	12.times do
+# 		if counter%2 == 0
+# 			mult = arr[counter-1].to_i*3
+# 			adder << mult
+# 		else
+# 			mult = arr[counter-1].to_i*1
+# 			adder << mult
+# 		end
+# 		counter += 1
+# 	end
+# 	# p adder
+# 	sum = adder.sum
+# 	y = sum % 10
+# 	x = 10 - y
+# 	checksum = x % 10
+# 	checksum.to_s
+# end
--- test_isbn.rb
+++ test_isbn.rb
@@ -6,59 +6,62 @@ def test_1_and_1
 	assert_equal(1,1)
 end
 
-def test_length_pass
-	assert_equal(true,isbn_validator("0471958697"))
-end
+# def test_length_pass
+# 	assert_equal(true,isbn_validator("0471958697"))
+# end
 
-def test_lenght_false
-	assert_equal(false,isbn_validator("00471958697"))
-end
+# def test_lenght_false
+# 	assert_equal(false,isbn_validator("00471958697"))
+# end
 
-def test_with_hypens_and_spaces
-	assert_equal(true,isbn_validator("0-321-14653-0"))
-end
+# def test_with_hypens_and_spaces
+# 	assert_equal(true,isbn_validator("0-321-14653-0"))
+# end
 
-def test_for_valid_keys
-	assert_equal(true,isbn_validator("0-321-14653-0"))
-end
+# def test_for_valid_keys
+# 	assert_equal(true,isbn_validator("0-321-14653-0"))
+# end
 
-def test_for_valid_keys_fail
-	assert_equal(false,isbn_validator("0-321-14z5y-0"))
-end
+# def test_for_valid_keys_fail
+# 	assert_equal(false,isbn_validator("0-321-14z5y-0"))
+# end
 
-def test_for_checksum_pass
-	assert_equal(true,isbn_validator("0471958697"))
-end
+# def test_for_checksum_pass
+# 	assert_equal(true,isbn_validator("0471958697"))
+# end
 
-def test_for_checksum_fail
-	assert_equal(false,isbn_validator("0471958698"))
-end
+# def test_for_checksum_fail
+# 	assert_equal(false,isbn_validator("0471958698"))
+# end
 
-def test_for_x_checksum
-	assert_equal(true,isbn_validator("877195869x"))
-end
+# def test_for_x_checksum
+# 	assert_equal(true,isbn_validator("877195869x"))
+# end
 
-def test_isbn13
-	assert_equal(true,isbn_validator("978-0-13-149505-0"))
-end
+# def test_isbn13
+# 	assert_equal(true,isbn_validator("978-0-13-149505-0"))
+# end
 
-def test_isbn3_incorrect_key
-	assert_equal(false,isbn_validator("978-0-x3-149x05-0"))
-end
+# def test_isbn3_incorrect_key
+# 	assert_equal(false,isbn_validator("978-0-x3-149x05-0"))
+# end
 
-def test_isbn13_1
-	assert_equal(true,isbn_validator("978-0-13-149505-0"))
-end
+# def test_isbn13_1
+# 	assert_equal(true,isbn_validator("978-0-13-149505-0"))
+# end
 
-def test_isbn13_1_fail
-	assert_equal(false,isbn_validator("978-0-13-149505-4"))
-end
+# def test_isbn13_1_fail
+# 	assert_equal(false,isbn_validator("978-0-13-149505-4"))
+# end
 
-def test_isbn13_2
-	assert_equal(true,isbn_validator("978 0 471 48648 0"))
-end
+# def test_isbn13_2
+# 	assert_equal(true,isbn_validator("978 0 471 48648 0"))
+# end
 
-def test_isbn13_2_failed
-	assert_equal(false,isbn_validator("978 0 471 48648 7"))
+# def test_isbn13_2_failed
+# 	assert_equal(false,isbn_validator("978 0 471 48648 7"))
+# end
+def test_key_checker_small
+	assert_equal(true,key_checker_small("0471958697"))
 end
 end
