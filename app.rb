c47b9ed7a3399816d4571eb32e6a8e6e60ca79ce
ScStew <scottmstewart2@gmail.com>
2017-09-05 11:59:15 -0400

    got acces to csv in app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require_relative "isbn.rb"
+require "csv"
 
 get "/" do
 	erb :opening_page
@@ -7,6 +8,7 @@ end
 
 post "/number" do
 	isbn = params[:isbn]
+	arr_of_arrs = CSV.read("isbn.csv")
 	redirect "/results?isbn=" + isbn
 end
 
@@ -14,4 +16,4 @@ get "/results" do
 	isbn = params[:isbn]
 	results = isbn_validator(isbn)
 	erb :results, locals:{isbn:isbn,results:results}
-end
+end	
