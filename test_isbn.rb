217f70e76e6d7fb484ff0770f97acbec49f73231
ScStew <scottmstewart2@gmail.com>
2017-09-06 09:14:55 -0400

    changed tests from true and false to valid and invalid

--- test_isbn.rb
+++ test_isbn.rb
@@ -7,59 +7,59 @@ def test_1_and_1
 end
 
 def test_length_pass
-	assert_equal(true,isbn_validator("0471958697"))
+	assert_equal("valid",isbn_validator("0471958697"))
 end
 
 def test_lenght_false
-	assert_equal(false,isbn_validator("00471958697"))
+	assert_equal("invalid",isbn_validator("00471958697"))
 end
 
 def test_with_hypens_and_spaces
-	assert_equal(true,isbn_validator("0-321-14653-0"))
+	assert_equal("valid",isbn_validator("0-321-14653-0"))
 end
 
 def test_for_valid_keys
-	assert_equal(true,isbn_validator("0-321-14653-0"))
+	assert_equal("valid",isbn_validator("0-321-14653-0"))
 end
 
 def test_for_valid_keys_fail
-	assert_equal(false,isbn_validator("0-321-14z5y-0"))
+	assert_equal("invalid",isbn_validator("0-321-14z5y-0"))
 end
 
 def test_for_checksum_pass
-	assert_equal(true,isbn_validator("0471958697"))
+	assert_equal("valid",isbn_validator("0471958697"))
 end
 
 def test_for_checksum_fail
-	assert_equal(false,isbn_validator("0471958698"))
+	assert_equal("invalid",isbn_validator("0471958698"))
 end
 
 def test_for_x_checksum
-	assert_equal(true,isbn_validator("877195869x"))
+	assert_equal("valid",isbn_validator("877195869x"))
 end
 
 def test_isbn13
-	assert_equal(true,isbn_validator("978-0-13-149505-0"))
+	assert_equal("valid",isbn_validator("978-0-13-149505-0"))
 end
 
 def test_isbn3_incorrect_key
-	assert_equal(false,isbn_validator("978-0-x3-149x05-0"))
+	assert_equal("invalid",isbn_validator("978-0-x3-149x05-0"))
 end
 
 def test_isbn13_1
-	assert_equal(true,isbn_validator("978-0-13-149505-0"))
+	assert_equal("valid",isbn_validator("978-0-13-149505-0"))
 end
 
 def test_isbn13_1_fail
-	assert_equal(false,isbn_validator("978-0-13-149505-4"))
+	assert_equal("invalid",isbn_validator("978-0-13-149505-4"))
 end
 
 def test_isbn13_2
-	assert_equal(true,isbn_validator("978 0 471 48648 0"))
+	assert_equal("valid",isbn_validator("978 0 471 48648 0"))
 end
 
 def test_isbn13_2_failed
-	assert_equal(false,isbn_validator("978 0 471 48648 7"))
+	assert_equal("invalid",isbn_validator("978 0 471 48648 7"))
 end
  
 def test_key_checker_small
